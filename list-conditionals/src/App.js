56be9e91d735ee2e6b53f39a5323794565c7706d
Uthaeus <romanlavery@gmail.com>
2018-11-21 19:15:15 -0700

    Created validation component

--- list-conditionals/src/App.js
+++ list-conditionals/src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import ValidationComponent from './ValidationComponent/ValidationComponent';
+
 class App extends Component {
+
+  state = {
+    input: 'Enter Input Here'
+  }
+
+
   render() {
     return (
       <div className="App">
-        
+        <input type='text' value={this.state.input} className='input' />
+        <ValidationComponent len={this.state.input.length} />
       </div>
     );
   }
